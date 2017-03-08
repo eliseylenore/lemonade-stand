2c6d1e4c54709be4796a1a1f539cfd5ce4453019
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-08 14:44:40 -0800

    Create inline form

--- Content/js/scripts.js
+++ Content/js/scripts.js
@@ -26,23 +26,13 @@ $(document).ready(function() {
     var costPerPitcher;
     var playerMoney = parseFloat($("#player-money").text()).toFixed(2);
     var newMoney = parseFloat($("#player-money").text()).toFixed(2);
-    $('input.pitcher').click(function() {
-        pitchers = parseFloat($('input.pitcher').val()).toFixed(2);
+    $('#pitcher').click(function() {
+        pitchers = parseFloat($('#pitcher').val()).toFixed(2);
         costPerPitcher = parseFloat($("#pitcher-price").text()).toFixed(2);
         if(pitchers > 0)
         {
             newMoney = playerMoney - (costPerPitcher * pitchers);
-
-            // if(newMoney > 0) {
-                $("#player-money").text(newMoney.toFixed(2));
-            // }
-            // else
-            // {
-            //
-            // }
-
-            console.log(costPerPitcher);
-            console.log(pitchers);
+            $("#player-money").text(newMoney.toFixed(2));
         }
     });
 
